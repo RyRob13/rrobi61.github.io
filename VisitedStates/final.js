c5ceca17f0cdd0553e29e1afba0aa8b3c3485f77
RyRob13 <35343413+RyRob13@users.noreply.github.com>
2018-04-17 15:10:54 -0500

    k

---
+++ VisitedStates/final.js
@@ -0,0 +1,72 @@
+let Finalmap = L.map('final').setView([39, -98], 4)
+
+let Graybasemap = 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}'
+let NatGeobasemap = 'https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}'
+let Streetbasemap = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}'
+
+let myGraymap1 = L.tileLayer(Graybasemap).addTo(Finalmap)
+let myLightmap1 = L.tileLayer(NatGeobasemap)
+let myStreetmap1 = L.tileLayer(Streetbasemap)
+
+let myBasemaps = {
+  'Gray Basemap' : myGraymap1,
+  'NatGeo Basemap' : myLightmap1,
+  'Street Basemap' : myStreetmap1
+}
+
+let newOrleans = L.marker([30, -90]).addTo(Finalmap)
+
+let opLayers = {
+  "New Orleans": newOrleans
+}
+
+L.control.layers(myBasemaps, opLayers).addTo(Finalmap)
+
+let stateStyle = function (state) {
+  let Travels = state.properties.STATE_NAME
+  let stateColor = 'blue'
+  if (Travels == "Louisiana") {
+    stateColor = 'red'
+  }
+  if (Travels == "Mississippi") {
+    stateColor = 'red'
+  }
+  if (Travels == "Texas") {
+    stateColor = 'red'
+  }
+  if (Travels == "Alabama") {
+    stateColor = 'red'
+  }
+  if (Travels == "Colorado") {
+    stateColor = 'red'
+  }
+  if (Travels == "North Carolina") {
+    stateColor = 'red'
+  }
+  if (Travels == "California") {
+    stateColor = 'red'
+  }
+  if (Travels == "Florida") {
+    stateColor = 'red'
+  }
+
+  let formatting = {
+    color: stateColor,
+    weight: 2,
+    fillOpacity: 0.2
+  }
+  return formatting
+}
+
+let createPopup = function (state, layer) {
+  let name = state.properties.STATE_NAME
+  let population = state.properties.POPULATION
+  layer.bindPopup('Poulation of ' + name + ': ' + population + '<br>Largest state (California): 38986171')
+}
+
+let stateOptions = {
+  style: stateStyle,
+  onEachFeature: createPopup
+}
+
+L.geoJSON(demographics, stateOptions).addTo(Finalmap)
